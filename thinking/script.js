c524a8be1cb5bf25cb7735331ef1401c6c514147
ZeraitoX <aeolusv3@gmail.com>
2024-11-24 16:15:50 +0900

    Update script.js

--- thinking/script.js
+++ thinking/script.js
@@ -1,1 +1,9 @@
+// script.js
+
+// 本をクリックした時のアクションを設定
+document.querySelectorAll('.book').forEach(book => {
+  book.addEventListener('click', function() {
+    alert('あなたは ' + book.getAttribute('data-text') + ' を選びました。');
+  });
+});
 
