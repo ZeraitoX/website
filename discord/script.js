51fa78360bfb26fc9955a7ae69af0b2f1e499c1a
ZeraitoX <aeolusv3@gmail.com>
2024-12-10 02:44:04 +0900

    Update script.js

--- discord/script.js
+++ discord/script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const joinButton = document.querySelector(".cta a");
+    joinButton.addEventListener("click", () => {
+        alert("You are being redirected to join the Discord server!");
+    });
+});
 
